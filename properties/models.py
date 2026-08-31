d79754113bdb16b84933e263fa87c4c519165ca6
ramtin <ramtin.windows.11pro@gmail.com>
2026-09-01 01:21:40 +0330

    feat(properties): implement hierarchical location and property coordinates

--- properties/models.py
+++ properties/models.py
@@ -89,6 +89,142 @@ class Property(models.Model):
         return self.title
 
 
+# ===================================================
+#                   Location
+# ===================================================
+class Country(models.Model):
+    # Identity
+    name = models.CharField(max_length=60, verbose_name="نام کشور")
+    code = models.CharField(max_length=6, verbose_name="کد کشور")
+    slug = models.SlugField(unique=True, verbose_name="اسلاگ")
+
+
+class Province(models.Model):
+    # Relationship
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE, related_name="provinces", verbose_name="کشور"
+    )
+    # Identity
+    name = models.CharField(max_length=25, verbose_name="نام استان")
+    code = models.CharField(max_length=6, verbose_name="کد استان")
+    slug = models.SlugField(unique=True, verbose_name="اسلاگ")
+
+
+class County(models.Model):
+    # Relationship
+    province = models.ForeignKey(
+        Province,
+        on_delete=models.CASCADE,
+        related_name="counties",
+        verbose_name="استان",
+    )
+    # Identity
+    name = models.CharField(max_length=20, verbose_name="نام شهرستان")
+    code = models.CharField(max_length=6, verbose_name="کد شهرستان")
+    slug = models.SlugField(unique=True, verbose_name="اسلاگ")
+
+
+class District(models.Model):
+    # Relationship
+    county = models.ForeignKey(
+        County,
+        on_delete=models.CASCADE,
+        related_name="rural_districts",
+        verbose_name="شهرستان",
+    )
+    # Identity
+    name = models.CharField(max_length=35, verbose_name="نام بخش")
+    code = models.CharField(max_length=6, verbose_name="کد بخش")
+    slug = models.SlugField(unique=True, verbose_name="اسلاگ")
+
+
+class RuralDistrict(models.Model):
+    # Relationship
+    district = models.ForeignKey(
+        District,
+        on_delete=models.CASCADE,
+        related_name="ruraldistricts",
+        verbose_name="بخش",
+    )
+    # Identity
+    name = models.CharField(max_length=50, verbose_name="نام دهستان")
+    code = models.CharField(max_length=6, verbose_name="کد دهستان")
+    slug = models.SlugField(unique=True, verbose_name="اسلاگ")
+
+
+class City(models.Model):
+    # Relationship
+    province = models.ForeignKey(
+        Province, on_delete=models.CASCADE, related_name="cities", verbose_name="استان"
+    )
+    county = models.ForeignKey(
+        County, on_delete=models.CASCADE, related_name="cities", verbose_name="شهرستان"
+    )
+    district = models.ForeignKey(
+        District, on_delete=models.CASCADE, related_name="cities", verbose_name="بخش"
+    )
+    # Identity
+    name = models.CharField(max_length=50, verbose_name="نام شهر")
+    code = models.CharField(max_length=6, verbose_name="کد شهر")
+    slug = models.SlugField(unique=True, verbose_name="اسلاگ")
+
+
+class PropertyLocation(models.Model):
+    # Relationship
+    property_obj = models.OneToOneField(
+        Property,
+        on_delete=models.CASCADE,
+        related_name="location",
+        verbose_name="موقعیت مکانی",
+    )
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE,
+        related_name="properties",
+        verbose_name="کشور",
+    )
+    province = models.ForeignKey(
+        Province,
+        on_delete=models.CASCADE,
+        related_name="properties",
+        verbose_name="استان",
+    )
+    county = models.ForeignKey(
+        County,
+        on_delete=models.CASCADE,
+        related_name="properties",
+        verbose_name="شهرستان",
+    )
+    district = models.ForeignKey(
+        District,
+        on_delete=models.CASCADE,
+        related_name="properties",
+        verbose_name="بخش",
+    )
+    rural_district = models.ForeignKey(
+        RuralDistrict,
+        on_delete=models.CASCADE,
+        related_name="properties",
+        verbose_name="دهستان",
+    )
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, related_name="properties", verbose_name="شهر"
+    )
+    # Address
+    address = models.CharField(
+        max_length=350, null=True, blank=True, verbose_name="آدرس تکمیلی"
+    )
+    postal_code = models.CharField(max_length=10, verbose_name="کد پستی")
+
+    # Geographic Coordinates
+    latitude = models.DecimalField(
+        max_digits=9, decimal_places=6, verbose_name="عرض جغرافیایی"
+    )
+    longitude = models.DecimalField(
+        max_digits=9, decimal_places=6, verbose_name="طول جغرافیایی"
+    )
+
+
 # ==================================================
 #                       Images
 # ==================================================
@@ -97,7 +233,7 @@ class Property(models.Model):
 class PropertyImage(models.Model):
 
     # Relationship
-    Property = models.ForeignKey(
+    propertyimg = models.ForeignKey(
         Property,
         on_delete=models.CASCADE,
         related_name="images",
